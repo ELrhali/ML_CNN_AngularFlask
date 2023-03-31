8c4dad08cc145ddba079d6bea05cc563c3bad5eb
ELrhali <mohammedelrhali98@gmail.com>
2023-03-31 18:23:47 +0000

    new mod

--- front/src/app/dashboard/dashboard.component.ts
+++ front/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,6 +13,10 @@ export class DashboardComponent implements OnInit {
   form: FormGroup |any;
   headers:HttpHeaders|any;
   ageData: any;
+  showCard: boolean = false;
+  @ViewChild('card') card!: ElementRef;
+
+
 
   constructor(private formBuilder:FormBuilder ,private http : HttpClient , private router: Router) { }
 
@@ -29,13 +33,22 @@ export class DashboardComponent implements OnInit {
 
 
   submit(): void{
+
     this.http.post('http://localhost:5000/pred',this.form.getRawValue()).subscribe(res =>{
       console.log(res);
       const resu= Object.values(res);
       this.ageData = parseFloat(resu[0]).toFixed(2);
+      this.showCard = true;
+
+  
     });
+   
   }
 
+  
+  hideCard() {
+    this.showCard = false;
+  }
 
       
     }
--- model/model/cnn.py
+++ model/model/cnn.py
@@ -6,10 +6,10 @@ from tensorflow import keras
 from sklearn.preprocessing import MinMaxScaler
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODELS = os.path.join(BASE_DIR, 'model') # we are now in src/model
+MODELS = os.path.join(BASE_DIR, 'model')
 
 def predict(a,s) : 
-    path = os.path.join(MODELS, "cnn.h5")
+    path = os.path.join(MODELS, "Model.h5")
     my_model = keras.models.load_model(path)
         
     array  =np.array([[a,s]])
@@ -23,15 +23,12 @@ def predict(a,s) :
 def normalizer(pred): 
     path = os.path.join(MODELS, "y-train.csv")
 
-    # Importing train set to normalizer
     y_train  = pd.read_csv(path,sep=',')
     y_train.set_index("Unnamed: 0", inplace = True)
     y_train.index.names = [""]
 
-    # Training normalizer
-    # create scaler
+  
     normer_y = MinMaxScaler()
-    # fit scaler on data
     normer_y.fit(y_train)
 
     pred = normer_y.inverse_transform(pred)
